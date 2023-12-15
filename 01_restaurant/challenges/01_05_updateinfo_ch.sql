7c813d1136d4fb46d7ac67665050f70456fa4d9f
Ismailduman <issmailduman@gmail.com>
2023-12-15 12:04:52 +0100

    update

--- 01_restaurant/challenges/01_05_updateinfo_ch.sql
+++ 01_restaurant/challenges/01_05_updateinfo_ch.sql
@@ -2,3 +2,10 @@
 
 -- Taylor Jenkins aus 27170 6th Ave., Washington, DC,
 -- ist umgezogen nach 74 Pine St., New York, NY.
+UPDATE Kunden SET Adresse='74 Pine St.',
+Stadt='New York',
+State='NY'
+WHERE KundenID=26;
+
+SELECT Adresse, Stadt, State FROM Kunden
+Where KundenID=26;
