0729499700b5439570c69e1e89c61dacbbdd12a2
Ismailduman <issmailduman@gmail.com>
2023-12-18 21:54:40 +0100

    11

--- 01_restaurant/challenges/01_11_lieblingsgerichtupdate_ch.sql
+++ 01_restaurant/challenges/01_11_lieblingsgerichtupdate_ch.sql
@@ -2,3 +2,12 @@
 
 -- Tragen Sie Cleo Goldwaters Lieblingsgericht 
 -- als "Quinoa Salmon Salad" ein.
+
+SELECT * FROM Kunden
+WHERE Kunden.Vorname='Cleo';
+
+SELECT SpeiseID From Speise
+Where Speise.Name='Quinoa Salmon Salad';
+
+UPDATE Kunden SET LieblingsGericht=(SELECT SpeiseID From Speise
+Where Speise.Name='Quinoa Salmon Salad');
