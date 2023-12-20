7163692af68e751b12602dc636e0c8b288037f9e
Ismailduman <issmailduman@gmail.com>
2023-12-20 23:15:08 +0100

    challenge 02-03

--- 02_bibliothek/challenges/02_03_ausleihen_ch.sql
+++ 02_bibliothek/challenges/02_03_ausleihen_ch.sql
@@ -2,3 +2,24 @@
 --  und „Great Expectations“ an Jack Vaan aus. 
 -- Das Ausleihdatum ist der 25. August 2022  
 --  und die Ausleihfrist endet am 8. September.
+SELECT BuchID From Buecher
+Where Barcode= '2855934983';
+
+SELECT BuchID FROM Buecher
+WHERE Barcode='5314419343';
+
+SELECT MitgliedsID FROM Mitglieder
+WHERE Email='jvaan@wisdompets.com';
+
+
+INSERT INTO Ausleihung (BuchID, MitgliedsID,AusleihDatum,Rueckgabefrist)
+VALUES ((SELECT BuchID FROM Buecher
+WHERE Barcode='5314419343'), (SELECT MitgliedsID FROM Mitglieder
+WHERE Email='jvaan@wisdompets.com'), '2023-12-20','2024-01-05'
+);
+
+SELECT * FROM Ausleihung
+JOIN Buecher ON Buecher.BuchID=Ausleihung.BuchID
+WHERE Ausleihung.MitgliedsID= (SELECT MitgliedsID FROM Mitglieder
+WHERE Email='jvaan@wisdompets.com')
+ORDER BY AusleihDatum DESC;
