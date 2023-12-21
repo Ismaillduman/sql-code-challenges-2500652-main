ff6b5a23b8a1c6baeeebefaf6951f154dd203632
Ismailduman <issmailduman@gmail.com>
2023-12-21 12:29:48 +0100

    02_04 and 02_05 select and update

--- 02_bibliothek/challenges/02_04_ausleihfrist_ch.sql
+++ 02_bibliothek/challenges/02_04_ausleihfrist_ch.sql
@@ -1,3 +1,11 @@
--- Erstellen Sie eine Liste von Büchern, deren Ausleihfrist am 13. Juli 2022 endet. 
+-- Erstellen Sie eine Liste von Büchern, deren Ausleihfrist am Sndet. 
 -- Diese Liste soll auch den Namen der ausleihenden Person, 
 -- sowie deren Email-Adresse enthalten. 
+
+SELECT Mitglieder.Vorname, Mitglieder.Email, Ausleihung.Rueckgabefrist, Buecher.Titel
+FROM Ausleihung JOIN Mitglieder ON Ausleihung.MitgliedsID=Mitglieder.MitgliedsID
+JOIN Buecher ON Buecher.BuchID=Ausleihung.BuchID
+WHERE Ausleihung.Rueckgabefrist='2024-01-05'
+AND Ausleihung.RueckgabeDatum IS NULL;
+
+
--- 02_bibliothek/challenges/02_05_buchrueckgabe_ch.sql
+++ 02_bibliothek/challenges/02_05_buchrueckgabe_ch.sql
@@ -5,3 +5,17 @@
 -- Buch 1: 6435968624
 -- Buch 2: 5677520613
 -- Buch 3: 8730298424
+
+UPDATE Ausleihung SET RueckgabeDatum ='2023-12-05'
+WHERE BuchID=(SELECT BuchID FROM Buecher WHERE Barcode='6435968624');
+
+UPDATE Ausleihung SET RueckgabeDatum ='2023-12-05'
+WHERE BuchID=(SELECT BuchID FROM Buecher WHERE Barcode='5677520613');
+
+UPDATE Ausleihung SET RueckgabeDatum ='2023-12-05'
+WHERE BuchID=(SELECT BuchID FROM Buecher WHERE Barcode='8730298424');
+
+SELECT Buecher.BuchID, Titel,RueckgabeDatum, Vorname FROM Ausleihung
+JOIN Buecher ON Ausleihung.BuchID=Buecher.BuchID
+JOIN Mitglieder ON Mitglieder.MitgliedsID=Ausleihung.MitgliedsID
+WHERE RueckgabeDatum='2023-12-05';
