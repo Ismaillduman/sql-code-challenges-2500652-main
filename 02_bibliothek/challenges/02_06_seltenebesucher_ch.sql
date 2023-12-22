c09999006e320d9c3ea2f50b29d2e8e0f4f689ff
Ismailduman <issmailduman@gmail.com>
2023-12-22 14:42:52 +0100

    02_06 seltenebesucher

--- 02_bibliothek/challenges/02_06_seltenebesucher_ch.sql
+++ 02_bibliothek/challenges/02_06_seltenebesucher_ch.sql
@@ -1,2 +1,8 @@
 -- Erstellen Sie eine Liste von Mitgliedern,  
 -- die am seltensten Bücher ausgeliehen haben. 
+
+SELECT COUNT(Ausleihung.AusleihID) as AnzahlAusleihung, Mitglieder.Email,Mitglieder.Vorname FROM Ausleihung
+JOIN Mitglieder ON Mitglieder.MitgliedsID=Ausleihung.MitgliedsID
+GROUP BY Ausleihung.MitgliedsID
+ORDER BY AnzahlAusleihung ASC
+Limit 15;
